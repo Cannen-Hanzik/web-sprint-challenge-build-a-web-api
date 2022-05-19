d64aba0786150058fb2cc6c97410edc3ba91c81b
Cannen <cannenhanzik2@gmail.com>
2022-05-19 11:58:06 -0500

    finished

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,43 +1,36 @@
-// add middlewares here related to actions
 const Actions = require('../actions/actions-model');
 
-function validateId(req, res, next) {
+
+function validateActionId(req, res, next) {
     Actions.get(req.params.id)
-        .then(act => {
-            if(act) {
-                req.existingAct = act;
+        .then(action => {
+            if(action) {
+                req.existingAction = action;
                 next();
             } else {
-                res.status(400).json({
-                    message: 'action not found'
-                })
+                res.status(404).json({ message: 'action not found' })
             }
         })
-} 
+}
 
-function validateAction(req, res, next) {
+
+function actionValidator(req, res, next) {
     console.log(req.body)
-    console .log(`description: ${req.body.notes == 'string'}`)
-    if(typeof req.body.description != 'string'
+    console.log(`description: ${req.body.notes == 'string'}`)
+    if( typeof req.body.description != 'string' 
         || req.body.description == ''
-        || req.body.notes == ''
+        || req.body.notes == '' 
         || typeof req.body.completed != 'boolean'
         || typeof req.body.project_id != 'number'
         || req.body.project_id == '' ) {
-            res.status(400).json({
-                message: 'missing require fields'
-            })
+            res.status(400).json({ message: 'missing required fields' })
+            return;
         }
-        req.actions = { 
-            project_id: req.body.project_id,
-            description: req.body.description,
-            notes: req.body.notes,
-            completed: req.body.completed
-        }
+        req.actions = { project_id: req.body.project_id, description: req.body.description, notes: req.body.notes, completed: req.body.completed }
         next();
 }
 
 module.exports = {
-    validateId,
-    validateAction
-}
+    validateActionId,
+    actionValidator
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,67 @@
 // Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+
+const {
+    validateProjectId,
+    projectValidator
+} = require('./projects-middleware');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(arr => {
+            res.status(200).json(arr)
+        })
+})
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.json(req.existingProject)
+})
+
+router.post('/', (req,res) => {
+    let { name, description } = req.body;
+    let projectAdded= req.body;
+    if(name && description) {
+        Projects.insert(projectAdded)
+            .then(project => {
+                res.status(201).json(project)
+            })
+    } else {
+        console.log(projectAdded)
+        res.status(400).json({ message: 'Please provide a name and description' })
+    }
+});
+
+router.put('/:id', validateProjectId, projectValidator, (req, res) => {
+    Projects.update(req.params.id, req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(error => {
+            res.status(400).json({ message: 'missing name, description or completed'})
+        })
+})
+
+router.delete('/:id', validateProjectId, (req, res) => {
+    Projects.remove(req.existingProject.id)
+        .then(() => {
+            res.status(200).json(req.existingProject);
+        })
+        .catch(error => {
+            res.status(404).json({ message: 'missing required id field'})
+        })
+})
+
+router.get('/:id/actions', validateProjectId, (req, res) => {
+    Projects.getProjectActions(req.params.id)
+        .then(arrayOfActions => {
+            res.status(200).json(arrayOfActions)
+        })
+        .catch(error => {
+            res.status(404).json({ message: 'missing required id field' })
+        })
+})
+
+module.exports = router
